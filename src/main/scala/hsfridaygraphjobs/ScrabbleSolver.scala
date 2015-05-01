2de38abafc8bf8d2a1bbb3d7e8d49571486964e5
Geoffrey Gilmore <ggilmore@mit.edu>
2015-05-01 18:17:30 -0400

    half-working detection

--- src/main/scala/hsfridaygraphjobs/ScrabbleSolver.scala
+++ src/main/scala/hsfridaygraphjobs/ScrabbleSolver.scala
@@ -5,43 +5,6 @@ import WordLetters._
 
 import scala.io.Source
 
-//sealed trait Trie{
-//  val children:Map[Char, Trie]
-//  val isWord:Boolean
-//
-//  def put(word:String):Trie ={
-//    val lword = word.toLowerCase
-//    if (lword.nonEmpty){
-//      if (this.children.contains(lword.charAt(0))) {
-//        val newNode:Trie = this.children(lword.charAt(0)).put(lword.drop(1))
-//        ScrabbleNode(children = this.children + (lword.charAt(0) -> newNode))
-//      }
-//      else {
-//        val newNode:Trie = ScrabbleNode().put(lword.drop(1))
-//        ScrabbleNode(children = this.children + (lword.charAt(0) -> newNode))
-//      }
-//    }
-//    else ScrabbleNode(isWord = true)
-//  }
-//
-//  def contains(word: String): Boolean = {
-//    val lword = word.toLowerCase
-//    if (lword.size > 1) {
-//      if (children.contains(lword.charAt(0))) this.children(lword.charAt(0)).contains(lword.drop(1))
-//      else false
-//    }
-//    else {
-//      if (children.contains(lword.charAt(0))) children(lword.charAt(0)).isWord else false
-//    }
-//  }
-//
-//}
-
-//case object Empty extends Trie{
-//  val children:Map[Char, Trie] = Map()
-//  val isWord = false
-////  def put(word:String):Trie = ScrabbleNode(children = Map(word.charAt(0) -> Empty().put(word.drop(1))))
-// }
 
 case class ScrabbleNode(isWord: Boolean = false, children: Map[Char, ScrabbleNode] = Map()) {
   def put(word:String):ScrabbleNode ={
@@ -61,7 +24,7 @@ case class ScrabbleNode(isWord: Boolean = false, children: Map[Char, ScrabbleNod
 
   def contains(word: String): Boolean = {
     val lword = word.toLowerCase
-    if (lword.size > 1) {
+    if (lword.length > 1) {
       if (children.contains(lword.charAt(0))) this.children(lword.charAt(0)).contains(lword.drop(1))
       else false
     }
@@ -78,13 +41,13 @@ object ScrabbleSolver extends App {
     'l' -> 1, 'o' -> 1, 'n' -> 1, 'q' -> 10, 'p' -> 3, 's' -> 1,
     'r' -> 1, 'u' -> 1, 't' -> 1, 'w' -> 4, 'v' -> 4, 'y' -> 4,
     'x' -> 8, 'z' -> 10)
-//  Source.fromFile(new File("src/main/resources/sowpods.txt")).getLines.foreach( x=>println(x))
+
   val lines = Source.fromFile(new File("/Users/gmgilmore/Dropbox/ScalaDev/HSFridayGraphJobsSBT/src/main/resources/sowpods.txt")).getLines
   var root:ScrabbleNode = ScrabbleNode()
   for (line <- lines) if (line.nonEmpty) {
-//    println(s"$root.put(${line.trim.toLowerCase})")
     root = root.put(line.trim.toLowerCase)
   }
+
 
   def getAllWords(tiles:String, root:ScrabbleNode):Set[String] = {
     def loop(tileLC:Map[Char, Int], currentPath:Seq[Char], currentNode:ScrabbleNode, foundWords:Set[String]):Set[String] = {
@@ -111,9 +74,8 @@ object ScrabbleSolver extends App {
     if (word.length == 1) Set(word,"")
     else powSet(word.drop(1)) union powSet(word.drop(1)).map(x => s"${word.charAt(0)}$x")
   }
-  
 
-  println(getWordsAndScores("hello"))
 
-//println("abc".toList.toSet.subsets.foldLeft(Set[String]())((s, subset) => subset.foldLeft("")((s, thisWord)))
+  println(getWordsAndScores("abcdefg"))
+
 }
