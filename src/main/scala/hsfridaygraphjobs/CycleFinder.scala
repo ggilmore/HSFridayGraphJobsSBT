9af64284079d6ecde3351a556e0beed66d11e9a6
Erisa Dervishi <erisa85d@gmail.com>
2015-05-05 16:45:49 -0400

    tested with large input graph

--- src/main/scala/hsfridaygraphjobs/CycleFinder.scala
+++ src/main/scala/hsfridaygraphjobs/CycleFinder.scala
@@ -1,10 +1,12 @@
 package hsfridaygraphjobs
 
 import scala.annotation.tailrec
+import scala.collection.mutable.Set
+import scala.io.Source
 
 object CycleFinder extends App{
 
-
+  val PATH = "src/main/scala/resources/connections.txt"
 
   class Digraph() {
     var adjacencyList: Map[Int, Set[Int]] = Map()
@@ -17,19 +19,22 @@ object CycleFinder extends App{
       println(s"loop($node:Option[Int], $evaluating:Set[Int], $visited:Set[Int])")
           if (!visited(node))
                 this.adjacencyList.getOrElse(node, Set()).foldLeft(false, Set[Int]()) { case ((bool, newVisitedSet), child) =>
+                  println("bool:" + bool)
                 println(s"Folding on $node's child: $child")
                 val (res, set) = loop(child, evaluating + node, visited + node)
-                println(s"Ending Fold on $node's child: $child")
-                if (!visited(child) && res) return (true, newVisitedSet union set)
-                else if (evaluating(child)) return (true, newVisitedSet)
-                else (false, newVisitedSet union set)
+                println(s"Ending Fold on $node's child: $child and result of looping with the child is $res")
+                if (!visited(child) && res)  return (true, newVisitedSet union set)
+                else if (evaluating(child))  return (true, newVisitedSet)
+                else (bool || false, newVisitedSet union set)
           }
+
       //we will come here either when the node we are evaluating is already visited, or it's a leaf (no children)
       (false, if (!visited(node)) visited + node else visited)
     }
 
     def hasCycle = (this.adjacencyList.keySet union this.adjacencyList.values.toSeq.flatten.toSet).foldLeft(false, Set[Int]()) { case ((boolean, set), node) => {
       val (res, newSet) = loop(node, Set(), set)
+      println(s"res value is $res")
       (boolean || res, newSet union set)
     }
     }
@@ -73,14 +78,25 @@ object CycleFinder extends App{
 
 
   val graph = new Digraph
-  graph.addEdge(1, 2)
-  graph.addEdge(1, 3)
-  graph.addEdge(4, 3)
-  graph.addEdge(4, 1)
+//  graph.addEdge(1, 2)
+//  graph.addEdge(1, 3)
+//  graph.addEdge(3, 4)
+//  graph.addEdge(4, 1)
 
+  createGraphFromFile(graph)
+  println(graph.adjacencyList)
   println(graph.hasCycle)
 
+  def createGraphFromFile(graph: Digraph) = {
+    Source.fromFile(PATH).getLines().toSeq.foreach(addEdges)
+    def addEdges(line: String) = {
+      if (line.trim.nonEmpty) {
+        val pair = line.split(" ")
+        graph.addEdge(pair(0).toInt, pair(1).toInt)
+      }
 
+    }
+}
 
 
 }
